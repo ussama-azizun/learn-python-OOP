e89849227e9abcf2c5dc16c21e948d347dbb5f7c
Azizun Ussama <56993220+ussama-azizun@users.noreply.github.com>
2023-02-12 00:42:36 +0700

    Encapsulation add

--- basic-oop-function.py
+++ basic-oop-function.py
@@ -1,4 +1,4 @@
-''' Constructor and Destructor 
+''' Functions 
  - isisstance : check whether object was create from class
  - dir : show attr and method
  - __class__ : show class name of object '''
@@ -17,3 +17,18 @@ class Employee:
         print(f"Employee name {self.name}")
         print(f"Employee salary {self.salary}")
         print(f"Employee Department {self.department} ")
+
+#%% Create object
+obj1 = Employee('Linda', 25000, 'Developer')
+obj2 = Employee('Loren', 50000, 'Manager')
+
+# %% Check was obj1  created from Employee class?
+print(isinstance(obj1, Employee))
+
+# %% Check obj1 method and attribute
+print(dir(obj1.show_data))
+
+# %% Check 
+print(obj1.__class__)
+
+# %%
