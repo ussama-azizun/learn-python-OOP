f90391ff1ffb2a743535ad5e0d0bc7947a991e79
Azizun Ussama <56993220+ussama-azizun@users.noreply.github.com>
2023-02-15 15:34:50 +0700

    Update basic-oop-variables.py

--- basic-oop-variables.py
+++ basic-oop-variables.py
@@ -1,22 +1,36 @@
 ''' Class and instance variable
-  - Class : (global) does not need to create object can access
+  - Class : (global) does not need to create object can access this variable
   - Instance : (local) require to create object  '''
 
 #%% Create class
 class Employee:
 
+    # Class variable
+    _min_salary = 12000
+    _max_salary = 50000
+
     # Method Constructor
     def __init__(self, name, salary, department):
-        # Set all attribute to private 
+        # Instance variable
         self.__name =  name 
         self.__salary = salary 
-        self.__department = department 
+        self._department = department 
 
     # Method show data change to protected
     def _show_data(self):
-        print(f"Employee name : {self.getName()}")
-        print(f"Employee salary : {self.getSalary()}")
-        print(f"Employee Department : {self.getDepartment()} ")
+        print(f"Employee name : {self.__name}")
+        print(f"Employee salary : {self.__salary}")
+        print(f"Employee Department : {self._department} ")
 
 # %% Create object
 obj1 = Employee('Linda', 25000, 'Developer')
+
+#%% Show data
+obj1._show_data()
+# %% Class variables
+# Does not need to create an object for access class variable
+print(f" Min salary {Employee._min_salary}")
+
+
+
+# %%
